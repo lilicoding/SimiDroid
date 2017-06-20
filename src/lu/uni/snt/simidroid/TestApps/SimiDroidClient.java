f563a8f5229d007dfa2be3e323e377aee32032f6
Jun GAO <gaojun0816@yahoo.com>
2017-06-20 13:49:23 +0200

    combined output

--- src/lu/uni/snt/simidroid/TestApps/SimiDroidClient.java
+++ src/lu/uni/snt/simidroid/TestApps/SimiDroidClient.java
@@ -1,5 +1,9 @@
 package lu.uni.snt.simidroid.TestApps;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import lu.uni.snt.simidroid.AppAbstract;
 import lu.uni.snt.simidroid.FeatureExtraction;
 import lu.uni.snt.simidroid.SimilarityAnalysis;
@@ -12,21 +16,36 @@ import lu.uni.snt.simidroid.plugin.resource.ResourceSimilarityAnalysis;
 
 public class SimiDroidClient {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FileNotFoundException 
 	{
 		Config.app1Path = args[0];
 		Config.app2Path = args[1];
 		Config.androidJars = args[2];
+		String toPath = args[3];
 		
-		//compare(new MethodFeatureExtraction(Config.app1Path), new MethodFeatureExtraction(Config.app2Path), new MethodSimilarityAnalysis());
-		compare(new MethodFeatureExtraction(Config.app1Path), new MethodFeatureExtraction(Config.app2Path), new MethodSimilarityAnalysis("res/libs91.txt"));
+		PrintStream nps = new PrintStream(new FileOutputStream("/dev/null"));
+		System.setOut(nps);
+//		System.setErr(nps);
 		
-		//compare(new ResourceFeatureExtraction(Config.app1Path), new ResourceFeatureExtraction(Config.app2Path), new ResourceSimilarityAnalysis());
-		
-		//compare(new ComponentFeatureExtraction(Config.app1Path), new ComponentFeatureExtraction(Config.app2Path), new ComponentSimilarityAnalysis());
+		// Compare Methods
+		compare(new MethodFeatureExtraction(Config.app1Path), 
+				new MethodFeatureExtraction(Config.app2Path), 
+//				new MethodSimilarityAnalysis("res/libs91.txt"),
+				new MethodSimilarityAnalysis(),
+				toPath + "_method");
+		// Compare Resources
+		compare(new ResourceFeatureExtraction(Config.app1Path), 
+				new ResourceFeatureExtraction(Config.app2Path), 
+				new ResourceSimilarityAnalysis(),
+				toPath + "_rec");
+		// Compare Components
+		compare(new ComponentFeatureExtraction(Config.app1Path), 
+				new ComponentFeatureExtraction(Config.app2Path), 
+				new ComponentSimilarityAnalysis(),
+				toPath + "_comp");
 	}
 
-	public static void compare(FeatureExtraction appA1, FeatureExtraction appA2, SimilarityAnalysis sa)
+	public static void compare(FeatureExtraction appA1, FeatureExtraction appA2, SimilarityAnalysis sa, String path)
 	{
 		appA1.extract();
 		AppAbstract aa1 = appA1.getAppAbstract();
@@ -38,6 +57,6 @@ public class SimiDroidClient {
 
 //		sa.output(true);
 //		sa.output(false);
-		sa.writeResultAsJSON("try");
+		sa.writeResultAsJSON(path);
 	}
 }
